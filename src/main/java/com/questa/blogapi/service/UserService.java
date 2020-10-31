f1c1ce710e7be65f4b054b046d8899dd799f3345
patelanilcsweng@gmail.com <patelanilcsweng@gmail.com>
2020-10-31 15:19:37 +0530

    jar commit

--- src/main/java/com/questa/blogapi/service/UserService.java
+++ src/main/java/com/questa/blogapi/service/UserService.java
@@ -12,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -55,6 +56,7 @@ public class UserService implements UserDetailsService {
 	@Autowired
 	private QuestionService questionService;
 	
+	@Lazy
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
 
