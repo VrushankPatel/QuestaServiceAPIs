abcb5c21b36b04533637029f5219c89a3d856ac4
patelanilcsweng@gmail.com <patelanilcsweng@gmail.com>
2020-10-31 23:05:18 +0530

    TYPE ERROR COMMIT

--- src/main/java/com/questa/blogapi/service/QuestionService.java
+++ src/main/java/com/questa/blogapi/service/QuestionService.java
@@ -202,7 +202,7 @@ public class QuestionService {
 		log.info("findAllBySubjectTopic ::" + questionSearch.toString());
 		if(questionSearch.getSearchType()!=null && questionSearch.getSearchType().equalsIgnoreCase("ALL")) {
 			return findAllQuestions(questionSearch.getUserId());
-		}else if(questionSearch.getSearchType()!=null && questionSearch.getSearchType().equalsIgnoreCase("WITHOUTANSER")) {
+		}else if(questionSearch.getSearchType()!=null && questionSearch.getSearchType().equalsIgnoreCase("WITHOUTANSWER")) {
 			List<Question> questionList = fetchAnswersAndFeedbacks(questionRepository.findBySubjectIgnoreCaseContainingAndTopicIgnoreCaseContainingAndQuestionDescIgnoreCaseContainingOrderByCreateDateDesc(questionSearch.getSubject(), questionSearch.getTopic(), questionSearch.getQuestionDesc()), questionSearch.getUserId());
 			List<Question> questionWithoutAnsList = new ArrayList<>();
 			questionList.forEach(que -> {
